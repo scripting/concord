aae1f04cb967251beb685db53d2b8fe1445742d8
Dave Winer <dave.winer@gmail.com>
2013-09-18 09:16:20 -0400

    Fixed numerous syntax errors in concordUtils.js
    The code was previously run in an environment that didn't require
    semicolons and curly braces.

--- concordUtils.js
+++ concordUtils.js
@@ -235,110 +235,147 @@ var defaultUtilsOutliner = "#outliner";
 			}
 		return s;
 		};
-	function stringKBytes (num) 
-		return (Math.round (num / 1024) + "K")
-	function sameDay (d1, d2) 
-		d1 = new Date (d1)
-		d2 = new Date (d2)
-		return ((d1.getFullYear () == d2.getFullYear ()) && (d1.getMonth () == d2.getMonth ()) && (d1.getDate () == d2.getDate ()))
-	function dayOfWeekToString (theDay) 
-		var weekday=new Array(7)
-		weekday[0]="Sunday"
-		weekday[1]="Monday"
-		weekday[2]="Tuesday"
-		weekday[3]="Wednesday"
-		weekday[4]="Thursday"
-		weekday[5]="Friday"
-		weekday[6]="Saturday"
-		return (weekday[theDay])
-	function monthToString (theMonthNum) //3/8/13 by DW
-		var names = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-		if (theMonthNum == undefined) 
-			var now = new Date ()
-			theMonthNum = now.getMonth ()
-		return (names [theMonthNum])
-	function timeString (when, flIncludeSeconds) 
+	function stringKBytes (num) }
+		return (Math.round (num / 1024) + "K");
+		}
+	function sameDay (d1, d2) {
+		d1 = new Date (d1);
+		d2 = new Date (d2);
+		return ((d1.getFullYear () == d2.getFullYear ()) && (d1.getMonth () == d2.getMonth ()) && (d1.getDate () == d2.getDate ()));
+		}
+	function dayOfWeekToString (theDay) {
+		var weekday=new Array (7);
+		weekday[0]="Sunday";
+		weekday[1]="Monday";
+		weekday[2]="Tuesday";
+		weekday[3]="Wednesday";
+		weekday[4]="Thursday";
+		weekday[5]="Friday";
+		weekday[6]="Saturday";
+		return (weekday [theDay])
+		}
+	function monthToString (theMonthNum) { //3/8/13 by DW
+		var names = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+		if (theMonthNum == undefined) {
+			var now = new Date ();
+			theMonthNum = now.getMonth ();
+			}
+		return (names [theMonthNum]);
+		}
+	function timeString (when, flIncludeSeconds) {
 		var hour = when.getHours (), minutes = when.getMinutes (), ampm = "AM", s
-		if (hour >= 12) 
-			ampm = "PM"
-		if (hour > 12) 
-			hour -= 12
-		if (hour == 0)
-			hour = 12
-		if (minutes < 10) 
-			minutes = "0" + minutes
-		if (flIncludeSeconds) 
-			var seconds = when.getSeconds ()
-			if (seconds < 10) 
-				seconds = "0" + seconds
-			s = hour + ":" + minutes + ":" + seconds + ampm
-		else 
-			s = hour + ":" + minutes + ampm
-		return (s)
-	function viewDate (when, flShortDayOfWeek) 
-		var now = new Date ()
-		when = new Date (when)
-		if (sameDay (when, now)) 
-			return (timeString (when, false)) //2/9/13 by DW
-		else 
-			var oneweek = 1000 * 60 * 60 * 24 * 7
-			var cutoff = now - oneweek
-			if (when > cutoff)  //within the last week
-				var s = dayOfWeekToString (when.getDay ())
-				if (flShortDayOfWeek) 
-					s = s.substring (0, 2)
-				return (s)
-			else 
-				return (when.toLocaleDateString ())
-	function isAlpha (ch) 
-		return (((ch >= 'a') && (ch <= 'z')) || ((ch >= 'A') && (ch <= 'Z')))
-	function isNumeric (ch) 
-		return ((ch >= '0') && (ch <= '9'))
-	function random (lower, upper) 
-		var range = upper - lower + 1
-		return (Math.floor ((Math.random () * range) + lower))
-	function getRandomPassword (ctchars) 
-		var s= "", ch
-		while (s.length < ctchars) 
-			ch = String.fromCharCode (random (33, 122))
-			if (isAlpha (ch) || isNumeric (ch)) 
-				s += ch
-		return (s.toLowerCase ())
-	function getCanonicalName (text) 
-		var s = "", ch, flNextUpper = false
+		if (hour >= 12) {
+			ampm = "PM";
+			}
+		if (hour > 12) {
+			hour -= 12;
+			}
+		if (hour == 0) {
+			hour = 12;
+			}
+		if (minutes < 10) {
+			minutes = "0" + minutes;
+			}
+		if (flIncludeSeconds) {
+			var seconds = when.getSeconds ();
+			if (seconds < 10) {
+				seconds = "0" + seconds;
+				}
+			s = hour + ":" + minutes + ":" + seconds + ampm;
+		else {
+			s = hour + ":" + minutes + ampm;
+			}
+		return (s);
+		}
+	function viewDate (when, flShortDayOfWeek) {
+		var now = new Date ();
+		when = new Date (when);
+		if (sameDay (when, now)) {
+			return (timeString (when, false)); 
+			}
+		else { 
+			var oneweek = 1000 * 60 * 60 * 24 * 7;
+			var cutoff = now - oneweek;
+			if (when > cutoff) { //within the last week
+				var s = dayOfWeekToString (when.getDay ());
+				if (flShortDayOfWeek) {
+					s = s.substring (0, 2);
+					}
+				return (s);
+				}
+			else {
+				return (when.toLocaleDateString ());
+				}
+	function isAlpha (ch) { 
+		return (((ch >= 'a') && (ch <= 'z')) || ((ch >= 'A') && (ch <= 'Z')));
+		}
+	function isNumeric (ch) {
+		return ((ch >= '0') && (ch <= '9'));
+		}
+	function random (lower, upper) {
+		var range = upper - lower + 1;
+		return (Math.floor ((Math.random () * range) + lower));
+		}
+	function getRandomPassword (ctchars) { 
+		var s= "", ch;
+		while (s.length < ctchars) {
+			ch = String.fromCharCode (random (33, 122));
+			if (isAlpha (ch) || isNumeric (ch)) {
+				s += ch;
+				}
+		return (s.toLowerCase ());
+		}
+	function getCanonicalName (text) {
+		var s = "", ch, flNextUpper = false;
 		text = stripMarkup (text); //6/30/13 by DW
-		for (var i = 0; i < text.length; i++) 
-			ch = text [i]
-			if (isAlpha (ch) || isNumeric (ch)) 
-				if (flNextUpper) 
-					ch = ch.toUpperCase ()
-					flNextUpper = false
-				else 
-					ch = ch.toLowerCase ()
-				s += ch
-			else 
-				if (ch == ' ') 
-					flNextUpper = true
-		return (s)
-	function secondsSince (when) //2/1/13 by DW
-		var now = new Date ()
-		return ((now - when) / 1000)
-	function yesterday (when)  //3/1/13 by DW
-		if (when == undefined) 
-			when = new Date ()
-		return (when - (24 * 60 * 60 * 1000))
-	function getURLParameter (name) 
-		return (decodeURI ((RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]))
-	function trimLeading (s, ch) //3/10/13 by DW
-		while (s.charAt (0) === ch) 
-			s = s.substr (1)
-		return (s)
-	function trimTrailing (s, ch) //3/10/13 by DW
-		while (s.charAt (s.length - 1) === ch) 
-			s = s.substr (0, s.length - 1)
-		return (s)
-	function trimWhitespace (s)  //3/10/13 by DW
-		return (trimLeading (trimTrailing (s, " "), " "))
+		for (var i = 0; i < text.length; i++) {
+			ch = text [i];
+			if (isAlpha (ch) || isNumeric (ch)) {
+				if (flNextUpper) {
+					ch = ch.toUpperCase ();
+					flNextUpper = false;
+					}
+				else {
+					ch = ch.toLowerCase ();
+					}
+				s += ch;
+				}
+			else { 
+				if (ch == ' ') {
+					flNextUpper = true;
+					}
+				}
+			}
+		return (s);
+		}
+	function secondsSince (when) { //2/1/13 by DW
+		var now = new Date ();
+		return ((now - when) / 1000);
+		}
+	function yesterday (when)  { //3/1/13 by DW
+		if (when == undefined) {
+			when = new Date ();
+			}
+		return (when - (24 * 60 * 60 * 1000));
+		}
+	function getURLParameter (name) {
+		return (decodeURI ((RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]));
+		}
+	function trimLeading (s, ch) { //3/10/13 by DW
+		while (s.charAt (0) === ch) { 
+			s = s.substr (1);
+			}
+		return (s);
+		}
+	function trimTrailing (s, ch) { //3/10/13 by DW
+		while (s.charAt (s.length - 1) === ch) {
+			s = s.substr (0, s.length - 1);
+			}
+		return (s);
+		}
+	function trimWhitespace (s)  { //3/10/13 by DW
+		return (trimLeading (trimTrailing (s, " "), " "));
+		}
 	function urlDecode (s) { //3/26/13 by DW
 		return (decodeURIComponent((s+'').replace(/\+/g, '%20')));
 		}
@@ -353,6 +390,7 @@ var defaultUtilsOutliner = "#outliner";
 		return (s.replace(/<\/?([a-z][a-z0-9]*)\b[^>]*>?/gi, ''));
 		}
 //misc
-	function deleteObject (id) 
-		var x = document.getElementById (id)
-		x.parentNode.removeChild (x)
+	function deleteObject (id) {
+		var x = document.getElementById (id);
+		x.parentNode.removeChild (x);
+		}
